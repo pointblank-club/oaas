859ffbf2cc97565dc42533ebedfa83c322290619
slashexx <dhruvpuri.35@gmail.com>
2025-10-16 20:12:08 +0530

    disable workflows to prevent auto deploy
    Signed-off-by: slashexx <dhruvpuri.35@gmail.com>

--- symbol-obfuscator/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/akashsingh/Desktop/llvm/symbol-obfuscator
-# Build directory: /Users/akashsingh/Desktop/llvm/symbol-obfuscator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

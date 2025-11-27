7c758299843cdc2d660f44cb84769a4e7a8f40a5
Zrahay <shubhangsinha44@gmail.com>
2025-11-27 22:23:25 +0530

    Pass udpates

--- mlir-obs/lib/PassRegistrations.cpp
+++ mlir-obs/lib/PassRegistrations.cpp
@@ -1,10 +1,27 @@
 #include "Obfuscator/Passes.h"
-#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
 #include "mlir/Pass/PassRegistry.h"
 
 using namespace mlir;
-using namespace mlir::obs;
 
-// Global registration - must be in global namespace for plugin loading
-static PassRegistration<StringEncryptPass> stringReg;
-static PassRegistration<SymbolObfuscatePass> symbolReg;
+namespace mlir {
+namespace obs {
+
+// Static registration of passes - this happens at plugin load time
+void registerObfuscationPasses() {
+  registerPass([]() -> std::unique_ptr<Pass> {
+    return std::make_unique<StringEncryptPass>();
+  });
+
+  registerPass([]() -> std::unique_ptr<Pass> {
+    return std::make_unique<SymbolObfuscatePass>();
+  });
+}
+
+} // namespace obs
+} // namespace mlir
+
+// MLIR plugin entry point
+extern "C" LLVM_ATTRIBUTE_WEAK void mlirRegisterPasses() {
+  mlir::obs::registerObfuscationPasses();
+}

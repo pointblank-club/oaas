d85b2ae0d9b365c4b3bbecdc654ca31f2a50df91
slashexx <dhruvpuri.35@gmail.com>
2025-12-06 22:34:48 +0530

    add complete dogbolt + CFG implementation
    Signed-off-by: slashexx <dhruvpuri.35@gmail.com>

--- cmd/llvm-obfuscator/frontend/src/components/index.ts
+++ cmd/llvm-obfuscator/frontend/src/components/index.ts
@@ -1,3 +1,4 @@
 export { GitHubIntegration } from './GitHubIntegration';
 export { FileTree } from './FileTree';
 export { TestResults } from './TestResults';
+export { CFGVisualizer } from './CFGVisualizer';
